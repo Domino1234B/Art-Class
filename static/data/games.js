25705589a2e147a2d084f0bcd69747e3a31bbb5d
Domino1234 <122507153+Domino1234B@users.noreply.github.com>
2023-09-06 19:54:23 -0700

    Update games.js

--- static/data/games.js
+++ static/data/games.js
@@ -45,8 +45,8 @@ var games = [
     {
     'id': 'minecraft.1.5.2',
     'title': 'Minecraft 1.5.2',
-    'url': 'https://github.com/Domino1234B/Art-Class/blob/main/static/data/Minecraft-1.5.2.html',
-    'image': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.reddit.com%2Fr%2Feaglercraft%2F%3Fcount%3D25%26page%3D1%26after%3Dt3_10dxsmn&psig=AOvVaw119cWvO2aK2JkDb8_fNdTG&ust=1694138312616000&source=images&cd=vfe&opi=89978449&ved=0CA0QjRxqFwoTCJjT4Zazl4EDFQAAAAAdAAAAABAD'
+    'url': '../main/static/data/Minecraft-1.5.2.html',
+    'image': 'https://github.com/Domino1234B/Art-Class/blob/main/static/data/Fav.jpg?raw=true'
   },
   {
     'id': 'thereisnogame',
