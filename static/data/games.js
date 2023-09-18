0de712d18145cab42cd5967cb0e805ed3b84a55b
Domino1234 <122507153+Domino1234B@users.noreply.github.com>
2023-09-18 16:14:57 -0700

    Update games.js

--- static/data/games.js
+++ static/data/games.js
@@ -84,7 +84,7 @@ var games = [
   {
     'id': 'roblox',
     'title': 'Roblox',
-    'url': 'https://universityequality.com?utm_source=proxy&utm_medium=browser&utm_campaign=nowgg_proxy',
+    'url': 'https://universityequality.com',
     'image': 'https://play-lh.googleusercontent.com/WNWZaxi9RdJKe2GQM3vqXIAkk69mnIl4Cc8EyZcir2SKlVOxeUv9tZGfNTmNaLC717Ht'
   },
   {
